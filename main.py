d374b911ece7a05b8f2c95488b13758660d37f5d
Kira <78575847+Syren0914@users.noreply.github.com>
2023-10-05 00:09:39 -0600

    Sending Reminder for Paydays using python

---
+++ main.py
@@ -0,0 +1,37 @@
+from datetime import date  # core python module
+import pandas as pd  # pip install pandas
+
+from send_email import send_email  
+
+
+# Public GoogleSheets url - not secure!
+SHEET_ID = "1pubXtXNQMhvrlooK-2yAMax2w820ZpnO2lVzSaPb-4I" 
+SHEET_NAME = "sheet1"  
+URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}"
+
+
+def load_df(url):
+    parse_dates = ["due_date", "reminder_date"]
+    df = pd.read_csv(url, parse_dates=parse_dates)
+    return df
+
+
+def query_data_and_send_emails(df):
+    present = date.today()
+    email_counter = 0
+    for _, row in df.iterrows():
+        if (present >= row["reminder_date"].date()) and (row["has_paid"] == "no"):
+            send_email(
+                subject=f'[Syren0914]',
+                receiver_email=row["email"],
+                name=row["name"],
+                due_date=row["due_date"].strftime("%d, %b %Y"),  
+                amount=row["amount"],
+            )
+            email_counter += 1
+    return f"Total Emails Sent: {email_counter}"
+
+
+df = load_df(URL)
+result = query_data_and_send_emails(df)
+print(result)
---
+++ send_email.py
@@ -0,0 +1,73 @@
+import os
+import smtplib
+from email.message import EmailMessage
+from email.utils import formataddr
+from pathlib import Path
+
+from dotenv import load_dotenv  
+
+PORT = 587  
+EMAIL_SERVER = "smtp.gmail.com"  
+
+# Load the environment variables
+current_dir = Path(__file__).resolve().parent if "__file__" in locals() else Path.cwd()
+envars = current_dir / ".env"
+load_dotenv(envars)
+
+# Read environment variables
+sender_email = os.getenv("EMAIL")
+password_email = os.getenv("PASSWORD")
+
+
+def send_email(subject, receiver_email, name, due_date, amount):
+    # Create the base text message.
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = formataddr(("Syren0914.", f"{sender_email}"))
+    msg["To"] = receiver_email
+    msg["BCC"] = sender_email
+
+    msg.set_content(
+        f"""\
+        Hi <strong> {name} </strong>,
+        I hope you are well.
+        I just wanted to drop you a quick note to remind you that tomorrow is PAD DAYYY {amount} USD.
+        Thank you for using our service. :D
+        Best regards
+        Syren0914
+        """
+    )
+    
+    msg.add_alternative(
+        f"""\
+    <html>
+      <body>
+        <p>Hi {name},</p>
+        <p>I hope you are well.</p>
+        <p>I just wanted to drop you a quick note to remind you that tomorrow is PAD DAYYY   <strong>{amount} USD</strong>.
+        <p> Thank you for using our service. :D</p>
+        <p></p>
+
+        <p>Best regards</p>
+        <p>Syren0914</p>
+      </body>
+    </html>
+    """,
+        subtype="html",
+    )
+
+    with smtplib.SMTP(EMAIL_SERVER, PORT) as server:
+        server.starttls()
+        server.login(sender_email, password_email)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
+
+
+if __name__ == "__main__":
+    send_email(
+        subject="Pay Day Reminder",
+        name="Erdene Batbayar",
+        receiver_email="msrgaming35@gmail.com",
+        due_date="13, October 2023",
+        
+        amount="1100$",
+    )
